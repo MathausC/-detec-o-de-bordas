dce4cfcd90ad2df568bd05800cc550d3f718ab1f
MathausC <mathaus273@gmail.com>
2022-08-25 10:58:01 -0300

    Criando Sobel

---
+++ src/deteccao_de_bordas.py
@@ -0,0 +1,46 @@
+import sys
+import cv2 as cv
+from cv2 import GaussianBlur
+import numpy as np
+
+def main(argv):
+    
+    window_name = ('Sobel Demo - Simple Edge Detector')
+    scale = 1
+    delta = 0
+    ddepth = cv.CV_16S
+    
+    
+    if len(argv) < 1:
+        return -1
+
+    src = cv.imread(argv[0], cv.IMREAD_COLOR)
+
+    if src is None:
+        print ('Error opening image: ' + argv[0])
+        return -1
+
+    gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
+    
+    
+    grad_x = cv.Sobel(gray, ddepth, 1, 0, ksize=3, scale=scale, delta=delta, borderType=cv.BORDER_DEFAULT)
+
+    grad_y = cv.Sobel(gray, ddepth, 0, 1, ksize=3, scale=scale, delta=delta, borderType=cv.BORDER_DEFAULT)
+    
+    
+    abs_grad_x = cv.convertScaleAbs(grad_x)
+    abs_grad_y = cv.convertScaleAbs(grad_y)
+    
+    
+    grad = cv.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)    
+    
+    cv.imshow(window_name, grad)
+    cv.waitKey(0)
+
+    kernel = 
+
+    grad_x = cv.filter2D(gray, ddepth, )
+    
+    return 0
+if __name__ == "__main__":
+    main(sys.argv[1:])
